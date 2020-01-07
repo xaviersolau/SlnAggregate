63570c79c273f8850aa33befded00838dedde6eb
Xavier Solau <xaviersolau@yahoo.fr>
2020-01-07 01:42:49 +0100

    Add a revert shadow project service.

---
+++ src/tools/SoloX.SlnAggregate/Services/IRevertShadowProjectService.cs
@@ -0,0 +1,28 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="IRevertShadowProjectService.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SoloX.SlnAggregate.Models;
+
+namespace SoloX.SlnAggregate.Services
+{
+    /// <summary>
+    /// IRevertShadowProjectService interface to revert a shadow project file back to the original project.
+    /// </summary>
+    public interface IRevertShadowProjectService
+    {
+        /// <summary>
+        /// Revert the shadow project file.
+        /// </summary>
+        /// <param name="aggregator">The aggregator containing the project.</param>
+        /// <param name="csProject">The shadow project used as input to revert to the original project file.</param>
+        /// <returns>The reverted project file path relative to the aggregator root path.</returns>
+        string RevertShadow(IAggregator aggregator, Project csProject);
+    }
+}
---
+++ src/tools/SoloX.SlnAggregate/Services/Impl/AShadowProjectService.cs
@@ -0,0 +1,67 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="AShadowProjectService.cs" company="SoloX Software">
+// Copyright (c) SoloX Software. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoloX.SlnAggregate.Services.Impl
+{
+    /// <summary>
+    /// AShadowProjectService implementation base.
+    /// </summary>
+    public class AShadowProjectService
+    {
+        private const string CsprojExt = ".csproj";
+        private const string ShadowCsprojExt = ".Shadow.csproj";
+
+        /// <summary>
+        /// Tells if the given path match a shadow project file.
+        /// </summary>
+        /// <param name="path">The path to match.</param>
+        /// <returns>True if the path match a shadow file name.</returns>
+        protected static bool IsShadowProjectFilePath(string path)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException($"The argument {nameof(path)} must not be null.");
+            }
+
+            return path.EndsWith(ShadowCsprojExt, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        /// <summary>
+        /// Convert the given project file to shadow project file.
+        /// </summary>
+        /// <param name="projectPath">The project file to convert.</param>
+        /// <returns>The shadow project file.</returns>
+        protected static string ConvertToShadowProjectFilePath(string projectPath)
+        {
+            if (projectPath == null)
+            {
+                throw new ArgumentNullException($"The argument {nameof(projectPath)} must not be null.");
+            }
+
+            return projectPath.Replace(CsprojExt, ShadowCsprojExt, StringComparison.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Convert the given shadow project file to the original project file.
+        /// </summary>
+        /// <param name="projectPath">The shadow project file to convert.</param>
+        /// <returns>The original project file.</returns>
+        protected static string ConvertFromShadowToProjectFilePath(string projectPath)
+        {
+            if (projectPath == null)
+            {
+                throw new ArgumentNullException($"The argument {nameof(projectPath)} must not be null.");
+            }
+
+            return projectPath.Replace(ShadowCsprojExt, CsprojExt, StringComparison.InvariantCulture);
+        }
+    }
+}
